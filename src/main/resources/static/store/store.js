3e7b2a9d3831bcc4513e493d2e6fff0e3c8b2f7a
Ivan <ivan_semenov@bk.ru>
2022-01-14 02:16:49 +0300

    homework 2_2 done with pagination

--- src/main/resources/static/store/store.js
+++ src/main/resources/static/store/store.js
@@ -6,12 +6,15 @@ angular.module('market-front').controller('storeController', function ($scope, $
             url: contextPath + 'api/v1/products',
             method: 'GET',
             params: {
+                p: pageIndex,
                 title_part: $scope.filter ? $scope.filter.title_part : null,
                 min_price: $scope.filter ? $scope.filter.min_price : null,
                 max_price: $scope.filter ? $scope.filter.max_price : null
             }
         }).then(function (response) {
+            console.log(response);
             $scope.ProductsPage = response.data;
+            $scope.paginationArray = $scope.generatePagesIndexes(1, $scope.ProductsPage.totalPages);
         });
     };
 
@@ -19,7 +22,15 @@ angular.module('market-front').controller('storeController', function ($scope, $
         $http.get(contextPath + 'api/v1/cart/add/' + productId)
             .then(function (response) {
             });
-    }
+    };
+
+    $scope.generatePagesIndexes = function(startPage, endPage){
+        let arr = [];
+        for (let i = startPage; i < endPage +1; i++){
+        arr.push(i);
+        };
+        return arr;
+    };
 
     $scope.loadProducts();
 });
